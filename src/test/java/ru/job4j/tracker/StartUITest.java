317f3fb3e9d950c1ca1adc1f8658611dfac58cbf
maksimmerkulov <merkulov.mv@outlook.com>
2025-05-05 18:23:59 +0500

    12.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Класс {@code StartUITest} представляет собой модульные тесты для проверки пользовательского интерфейса
- * {@link StartUI}, включая действия создания, замены, удаления заявок и выхода из программы.
+ * Класс {@code StartUITest} представляет собой модульные тесты для пользовательского интерфейса {@link StartUI}.
  *
- * <p>Используется для проверки правильности обработки интерактивного ввода через {@link Input}
- * и корректности выполнения операций над заявками в {@link Tracker}.</p>
+ * <p>Используется для проверки корректной работы пользовательского интерфейса,
+ * включая команды создания, изменения, удаления и поиска заявок в хранилище {@link Tracker},
+ * а также проверки корректности текстового вывода.</p>
  *
  * <p><b>Пример использования:</b></p>
  * <pre>{@code
@@ -21,19 +21,22 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * <p><b>Пример вывода:</b></p>
  * <pre>{@code
- * (Проверки проходят без исключений и ошибок утверждений)
+ * Меню:
+ * 0. Завершить программу
+ * === Завершение программы ===
  * }</pre>
  *
  * @author Maksim Merkulov
- * @version 1.1
+ * @version 1.2
  */
 class StartUITest {
 
     /**
      * Проверяет создание новой заявки пользователем.
      *
-     * <p>Сценарий: пользователь выбирает действие создания, вводит название и завершает программу.
-     * Ожидается, что заявка будет добавлена в хранилище с заданным именем.</p>
+     * <p>Сценарий: пользователь выбирает действие создания, вводит название и завершает программу.</p>
+     *
+     * <p>Ожидание: заявка будет добавлена в хранилище с заданным именем.</p>
      */
     @Test
     void whenCreateItem() {
@@ -54,7 +57,9 @@ class StartUITest {
      * Проверяет замену существующей заявки.
      *
      * <p>Сценарий: добавляется заявка, затем пользователь выбирает операцию замены,
-     * вводит ID заявки и новое имя. Ожидается, что имя будет обновлено.</p>
+     * вводит ID заявки и новое имя.</p>
+     *
+     * <p>Ожидание: имя существующей заявки будет обновлено.</p>
      */
     @Test
     void whenReplaceItem() {
@@ -76,8 +81,9 @@ class StartUITest {
     /**
      * Проверяет удаление существующей заявки.
      *
-     * <p>Сценарий: добавляется заявка, затем пользователь выбирает удаление
-     * и вводит ее ID. После выполнения заявка должна быть удалена из хранилища.</p>
+     * <p>Сценарий: добавляется заявка, затем пользователь выбирает удаление и вводит ее ID.</p>
+     *
+     * <p>Ожидание: заявка будет удалена из хранилища.</p>
      */
     @Test
     void whenDeleteItem() {
@@ -98,8 +104,9 @@ class StartUITest {
     /**
      * Проверяет корректное завершение программы по команде выхода.
      *
-     * <p>Сценарий: пользователь выбирает действие "Завершить программу". Ожидается, что программа
-     * отобразит соответствующее сообщение и завершит выполнение.</p>
+     * <p>Сценарий: пользователь выбирает действие "Завершить программу".</p>
+     *
+     * <p>Ожидание: программа отобразит сообщение о завершении и завершит выполнение.</p>
      */
     @Test
     void whenExit() {
@@ -116,6 +123,148 @@ class StartUITest {
                 "Меню:" + System.lineSeparator()
                         + "0. Завершить программу" + System.lineSeparator()
                         + "=== Завершение программы ===" + System.lineSeparator()
+        );
+    }
+
+    /**
+     * Проверяет корректность вывода при успешной замене заявки.
+     *
+     * <p>Сценарий: добавляется заявка, затем она заменяется по ID на новую с другим именем.</p>
+     *
+     * <p>Ожидание: в выводе будет подтверждение успешной замены и корректное меню до и после действия.</p>
+     */
+    @Test
+    void whenReplaceItemTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String replaceName = "New Test Name";
+        Input input = new MockInput(
+                new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new ReplaceAction(output),
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Изменить заявку" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Редактирование заявки ===" + ln
+                        + "Заявка изменена успешно." + ln
+                        + "Меню:" + ln
+                        + "0. Изменить заявку" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
+
+    /**
+     * Проверяет корректность вывода всех заявок.
+     *
+     * <p>Сценарий: добавляются две заявки, затем вызывается команда показа всех заявок.</p>
+     *
+     * <p>Ожидание: обе заявки будут отображены в выводе.</p>
+     */
+    @Test
+    void whenFindAllActionTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item("test2"));
+        Input input = new MockInput(
+                new String[] {"0", "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindAllAction(output),
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Показать все заявки" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Вывод всех заявок ===" + ln
+                        + one + ln
+                        + two + ln
+                        + "Меню:" + ln
+                        + "0. Показать все заявки" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
+
+    /**
+     * Проверяет корректность вывода заявки по ID.
+     *
+     * <p>Сценарий: добавляется заявка, затем вызывается команда поиска по ее ID.</p>
+     *
+     * <p>Ожидание: заявка с указанным ее ID будет найдена и выведена на экран.</p>
+     */
+    @Test
+    void whenFindByIdActionTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input input = new MockInput(
+                new String[] {"0", String.valueOf(one.getId()), "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindByIdAction(output),
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Показать заявку по id" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Вывод заявки по id ===" + ln
+                        + one + ln
+                        + "Меню:" + ln
+                        + "0. Показать заявку по id" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
+
+    /**
+     * Проверяет корректность вывода заявок по имени.
+     *
+     * <p>Сценарий: добавляются две заявки с одинаковым именем. Пользователь выбирает команду
+     * поиска по имени и вводит это имя.</p>
+     *
+     * <p>Ожидание: обе заявки с совпадающим именем будут найдены и выведены на экран.</p>
+     */
+    @Test
+    void whenFindByNameActionTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item("test1"));
+        Input input = new MockInput(
+                new String[] {"0", one.getName(), "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindByNameAction(output),
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Показать заявки по имени" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Вывод заявок по имени ===" + ln
+                        + one + ln
+                        + two + ln
+                        + "Меню:" + ln
+                        + "0. Показать заявки по имени" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
         );
     }
 }
