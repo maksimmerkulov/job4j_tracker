c0c7bf40a36c5941547b4f13c709dc9a3a969a13
maksimmerkulov <merkulov.mv@outlook.com>
2025-06-20 15:29:59 +0500

    6.2. Локальные и анонимные внутренние классы [#504952 #539399]

---
+++ src/main/java/ru/job4j/oop/BirdMain.java
@@ -0,0 +1,57 @@
+package ru.job4j.oop;
+
+/**
+ * Класс {@code BirdMain} демонстрирует создание и использование
+ * анонимного класса на основе класса {@link Bird}.
+ *
+ * <p>Метод {@link Bird#fly()} переопределяется внутри анонимного класса.
+ * Также добавляется собственный метод {@code flyInside()}, который доступен
+ * только внутри анонимного класса.</p>
+ *
+ * <p><b>Пример использования:</b></p>
+ * <pre>{@code
+ * Bird bird = new Bird() {
+ *     @Override
+ *     public void fly() {
+ *         System.out.println("Попугай пролетает над лужайкой");
+ *         flyInside();
+ *     }
+ *
+ *     public void flyInside() {
+ *         System.out.println("Попугай летает только внутри анонимного класса");
+ *     }
+ * };
+ * bird.fly();
+ * }</pre>
+ *
+ * <p><b>Пример вывода:</b></p>
+ * <pre>{@code
+ * Попугай пролетает над лужайкой
+ * Попугай летает только внутри анонимного класса
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class BirdMain {
+
+    /**
+     * Точка входа в программу.
+     *
+     * @param args Аргументы командной строки (не используются).
+     */
+    public static void main(String[] args) {
+        Bird bird = new Bird() {
+            @Override
+            public void fly() {
+                System.out.println("Попугай пролетает над лужайкой");
+                flyInside();
+            }
+
+            public void flyInside() {
+                System.out.println("Попугай летает только внутри анонимного класса");
+            }
+        };
+        bird.fly();
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Local.java
@@ -0,0 +1,61 @@
+package ru.job4j.oop;
+
+/**
+ * Класс {@code Local} демонстрирует использование локального внутреннего класса
+ * внутри метода экземпляра.
+ *
+ * <p>Локальный класс {@code FullName}, определенный в методе {@link #getFullName()},
+ * использует поле внешнего класса и локальную переменную метода для формирования полного имени.</p>
+ *
+ * <p><b>Пример использования:</b></p>
+ * <pre>{@code
+ * Local local = new Local();
+ * local.getFullName();
+ * }</pre>
+ *
+ * <p><b>Пример вывода:</b></p>
+ * <pre>{@code
+ * Maksim Merkulov
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Local {
+
+    /**
+     * Имя, доступное для использования во внутреннем классе.
+     */
+    private String name = "Maksim";
+
+    /**
+     * Метод {@code getFullName()} демонстрирует использование локального внутреннего класса.
+     *
+     * <p>Внутри метода создается локальный класс {@code FullName},
+     * который объединяет имя и фамилию и выводит результат в консоль.</p>
+     */
+    public void getFullName() {
+        final String surname = "Merkulov";
+
+        class FullName {
+            public static final String PATRONYMIC = "Empty";
+
+            public void printFullName() {
+                System.out.println(name + " " + surname);
+            }
+        }
+
+        FullName fullName = new FullName();
+        fullName.printFullName();
+    }
+
+    /**
+     * Точка входа в программу.
+     *
+     * @param args Аргументы командной строки (не используются).
+     */
+    public static void main(String[] args) {
+        Local local = new Local();
+        local.getFullName();
+    }
+}
