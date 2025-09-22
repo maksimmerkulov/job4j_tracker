7af7b3e0c6677e7220bc9b10aff2d3fac527f088
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-22 14:30:00 +0500

    Checkstyle rules [#504981 #537603]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,30 @@
+package ru.job4j.checkstyle;
+
+/**
+ * Demonstrates code formatting according to Checkstyle rules.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Broken {
+    public static final String NEW_VALUE = "";
+
+    private final int sizeOfEmpty = 10;
+    private String name;
+    private String surname;
+
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
