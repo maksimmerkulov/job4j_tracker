a0e4a7f58e09f6ce90d5fa8dd67bb9bfc7ab081e
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-23 14:30:00 +0500

    Object creation [#165452 #537711]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+/**
+ * Demonstrates the creation of {@link Cat} objects.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Cat {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+/**
+ * Demonstrates the creation of {@link Dog} objects.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Dog {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
