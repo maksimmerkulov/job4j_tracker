6339935c0ac8bb41040207263f7245090147089c
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-24 16:00:00 +0500

    Constructor overloading [#173890 #538193]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -13,13 +13,17 @@ package ru.job4j.oop;
  * SSD: 500 GB
  * CPU model: Intel Core I7-10700K
  *
- * Multi-monitor: true
+ * Multi-monitor: false
  * SSD: 256 GB
  * CPU model: AMD Ryzen 5 3600
+ *
+ * Multi-monitor: true
+ * SSD: 512 GB
+ * CPU model: AMD Ryzen 7 3700X
  * }</pre>
  *
  * @author Maksim Merkulov
- * @version 1.0
+ * @version 1.1
  */
 public class Computer {
 
@@ -45,6 +49,17 @@ public class Computer {
     }
 
     /**
+     * Creates a new {@code Computer} with the specified SSD and CPU.
+     *
+     * @param ssd capacity in GB
+     * @param cpu model name
+     */
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    /**
      * Creates a new {@code Computer} with the specified parameters.
      *
      * @param multiMonitor support for multiple monitors
@@ -54,6 +69,20 @@ public class Computer {
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    /**
+     * Creates a new {@code Computer} with the specified parameters,
+     * using a {@code double} value for SSD capacity.
+     *
+     * @param multiMonitor support for multiple monitors
+     * @param ssd          capacity in GB (will be cast to int)
+     * @param cpu          model name
+     */
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
         this.cpu = cpu;
     }
 
@@ -73,11 +102,13 @@ public class Computer {
      * @param args command-line arguments; not used
      */
     public static void main(String[] args) {
-        Computer defaultComputer = new Computer();
-        defaultComputer.printInfo();
-        Computer intelComputer = new Computer(true, 500, "Intel Core I7-10700K");
-        intelComputer.printInfo();
-        Computer amdComputer = new Computer(true, 256, "AMD Ryzen 5 3600");
-        amdComputer.printInfo();
+        Computer first = new Computer();
+        first.printInfo();
+        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
+        second.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
+        Computer fourth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        fourth.printInfo();
     }
 }
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,21 +1,81 @@
 package ru.job4j.tracker;
 
+/**
+ * An item in the tracker system.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
 public class Item {
+
+    /**
+     * Unique identifier of the item.
+     */
     private int id;
+
+    /**
+     * Name of the item.
+     */
     private String name;
 
+    /**
+     * Creates a new {@code Item} with default values.
+     */
+    public Item() {
+    }
+
+    /**
+     * Creates a new {@code Item} with the specified name.
+     *
+     * @param name the item name
+     */
+    public Item(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Creates a new {@code Item} with the specified id and name.
+     *
+     * @param id   the item identifier
+     * @param name the item name
+     */
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Returns the unique identifier of the item.
+     *
+     * @return the item id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the item.
+     *
+     * @param id the item id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Returns the name of the item.
+     *
+     * @return the item name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the item.
+     *
+     * @param name the item name
+     */
     public void setName(String name) {
         this.name = name;
     }
