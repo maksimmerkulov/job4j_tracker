ceddd1a54050ef7db107eb58cbc6f7e0b363a828
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-26 16:00:00 +0500

    Variable Scope [#180147 #538798]

---
+++ src/main/java/ru/job4j/max/Reduce.java
@@ -0,0 +1,53 @@
+package ru.job4j.max;
+
+/**
+ * A model demonstrating the difference between class fields and local
+ * variables.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * 1
+ * 2
+ * 3
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Reduce {
+
+    /**
+     * The internal storage for the integer array of the reduce.
+     */
+    private int[] array;
+
+    /**
+     * Assigns the specified array to the internal field.
+     *
+     * @param array the array to store
+     */
+    public void to(int[] array) {
+        this.array = array;
+    }
+
+    /**
+     * Prints all elements of the stored array to the console.
+     */
+    public void print() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        Reduce reduce = new Reduce();
+        reduce.to(array);
+        reduce.print();
+    }
+}
