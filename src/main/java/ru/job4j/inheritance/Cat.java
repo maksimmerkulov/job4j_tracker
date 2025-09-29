a9559e1e0ac56fe963755e72b3cd4f286403af8d
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-29 14:30:00 +0500

    What is inheritance [#3813 #539909]

---
+++ src/main/java/ru/job4j/inheritance/Cat.java
@@ -0,0 +1,19 @@
+package ru.job4j.inheritance;
+
+/**
+ * Represents a cat in the inheritance hierarchy.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class Cat {
+
+    /**
+     * Indicates whether the cat can purr.
+     *
+     * @return {@code true} if the cat can purr, {@code false} otherwise
+     */
+    public boolean canPurr() {
+        return true;
+    }
+}
