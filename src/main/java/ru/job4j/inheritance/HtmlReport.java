143d5d1fa4bf49069b40ac70ea446235132eaf0a
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-29 17:00:00 +0500

    @Override Annotation [#174891 #540360]

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -4,7 +4,7 @@ package ru.job4j.inheritance;
  * An HTML report model extending the base {@link TextReport} generator.
  *
  * @author Maksim Merkulov
- * @version 1.0
+ * @version 1.1
  */
 public class HtmlReport extends TextReport {
 
@@ -15,6 +15,7 @@ public class HtmlReport extends TextReport {
      * @param body the report body
      * @return the formatted HTML report string
      */
+    @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>"
                 + "<br/>"
