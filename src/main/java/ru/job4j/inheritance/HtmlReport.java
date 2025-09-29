6f5a58d7e439503c0333a99b7877e273b19b1671
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-29 15:30:00 +0500

    Method overriding [#174890 #539961]

---
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -0,0 +1,23 @@
+package ru.job4j.inheritance;
+
+/**
+ * Generates reports in HTML format.
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class HtmlReport extends TextReport {
+
+    /**
+     * Generates a report with HTML tags.
+     *
+     * @param name the report name
+     * @param body the report body
+     * @return the formatted HTML report string
+     */
+    public String generate(String name, String body) {
+        return "<h1>" + name + "</h1>"
+                + "<br/>"
+                + "<span>" + body + "</span>";
+    }
+}
---
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -0,0 +1,26 @@
+package ru.job4j.inheritance;
+
+/**
+ * Demonstrates the usage of the {@link HtmlReport} class.
+ *
+ * <p>Example output:
+ * <pre>{@code
+ * <h1>Report's name</h1><br/><span>Report's body</span>
+ * }</pre>
+ *
+ * @author Maksim Merkulov
+ * @version 1.0
+ */
+public class ReportUsage {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments; not used
+     */
+    public static void main(String[] args) {
+        HtmlReport report = new HtmlReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+    }
+}
