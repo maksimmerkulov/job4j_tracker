5dce07ae092c70065ec693640f1109afac44c473
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-29 17:30:00 +0500

    Date. Displaying Date [#310017 #540363]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,23 +1,29 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 /**
- * An item data model component which is used inside the {@link Tracker}
- * storage.
+ * An item model in the tracker system.
  *
  * @author Maksim Merkulov
- * @version 1.0
+ * @version 1.1
  */
 public class Item {
 
     /**
-     * Unique identifier of the item.
+     * The id of the item.
      */
     private int id;
 
     /**
-     * Name of the item.
+     * The name of the item.
      */
     private String name;
+
+    /**
+     * The creation timestamp of the item.
+     */
+    private LocalDateTime created = LocalDateTime.now();
 
     /**
      * Creates a new {@code Item} with default values.
@@ -28,7 +34,7 @@ public class Item {
     /**
      * Creates a new {@code Item} with the specified name.
      *
-     * @param name the item name
+     * @param name the name
      */
     public Item(String name) {
         this.name = name;
@@ -37,8 +43,8 @@ public class Item {
     /**
      * Creates a new {@code Item} with the specified id and name.
      *
-     * @param id   the item identifier
-     * @param name the item name
+     * @param id   the identifier
+     * @param name the name
      */
     public Item(int id, String name) {
         this.id = id;
@@ -48,7 +54,7 @@ public class Item {
     /**
      * Returns the unique identifier of the item.
      *
-     * @return the item id
+     * @return the item id value
      */
     public int getId() {
         return id;
@@ -66,7 +72,7 @@ public class Item {
     /**
      * Returns the name of the item.
      *
-     * @return the item name
+     * @return the item name value
      */
     public String getName() {
         return name;
@@ -79,5 +85,14 @@ public class Item {
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * Returns the creation date and time of the item.
+     *
+     * @return the creation timestamp value
+     */
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
