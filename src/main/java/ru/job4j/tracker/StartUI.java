88b58ef85bf44950b0dbe8534f10193c4556d0dc
maksimmerkulov <merkulov.mv@outlook.com>
2025-10-06 15:30:00 +0500

    StartUI class implementation. Outputting all items [#500745 #541579]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -19,10 +19,47 @@ import java.util.Scanner;
  * === Add new item ===
  * Enter name: Fix bugs
  * Added item: Item{id=1, name='Fix bugs', created=06-October-Monday-2025 15:00:00}
+ * Menu:
+ * 0. Add new item
+ * 1. Show all items
+ * 2. Edit item
+ * 3. Delete item
+ * 4. Find item by id
+ * 5. Find items by name
+ * 6. Exit program
+ * Select: 0
+ * === Add new item ===
+ * Enter name: Refactor code
+ * Added item: Item{id=2, name='Refactor code', created=06-October-Monday-2025 15:15:00}
+ * Menu:
+ * 0. Add new item
+ * 1. Show all items
+ * 2. Edit item
+ * 3. Delete item
+ * 4. Find item by id
+ * 5. Find items by name
+ * 6. Exit program
+ * Select: 0
+ * === Add new item ===
+ * Enter name: Write tests
+ * Added item: Item{id=3, name='Write tests', created=06-October-Monday-2025 15:30:00}
+ * Menu:
+ * 0. Add new item
+ * 1. Show all items
+ * 2. Edit item
+ * 3. Delete item
+ * 4. Find item by id
+ * 5. Find items by name
+ * 6. Exit program
+ * Select: 1
+ * === Show all items ===
+ * Item{id=1, name='Fix bugs', created=06-October-Monday-2025 15:00:00}
+ * Item{id=2, name='Refactor code', created=06-October-Monday-2025 15:15:00}
+ * Item{id=3, name='Write tests', created=06-October-Monday-2025 15:30:00}
  * }</pre>
  *
  * @author Maksim Merkulov
- * @version 1.3
+ * @version 1.4
  */
 public class StartUI {
 
@@ -45,6 +82,16 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Added item: " + item);
+            } else if (select == 1) {
+                System.out.println("=== Show all items ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Storage does not contain items yet");
+                }
             } else if (select == 6) {
                 run = false;
             }
